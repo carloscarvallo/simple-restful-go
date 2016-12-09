0455ca23830049b47cbbbbba2c297ba09028fa48
carloscarvallo <carloscarvallo87@gmail.com>
2016-12-08 21:44:14 -0300

    defining JSON encoder

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 
+	"encoding/json"
+
 	"github.com/gorilla/mux"
 )
 
@@ -26,7 +28,8 @@ func GetPersonEndpoint(w http.ResponseWritter, req *http.Request) {
 }
 
 func GetPeopleEndpoint(w http.ResponseWritter, req *http.Request) {
-
+	// taking the slices of strucs of people and encoding it to JSON
+	json.NewEncoder(w).Encode(people)
 }
 
 func CreatePersonEndpoint(w http.ResponseWritter, req *http.Request) {
@@ -40,6 +43,7 @@ func DeletePersonEndpoint(w http.ResponseWritter, req *http.Request) {
 func main() {
 	router := mux.NewRouter()
 	people = append(people, Person{ID: "1", FirstName: "Carlos", Lastname: "Carv", Address: &Address{City: "Asuncion", State: "Central"}})
+	people = append(people, Person{ID: "2", FirstName: "Maria", Lastname: "Mont"})
 	router.HandleFunc("/people", GetPeopleEndpoint).Method("GET")
 	router.HandleFunc("/people/{id}", GetPersonEndpoint).Method("GET")
 	router.HandleFunc("/people/{id}", CreatePersonEndpoint).Method("POST")
