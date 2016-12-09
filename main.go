c68d08d01df16ee98a6637e76b3d32c5c6310fa4
carloscarvallo <carloscarvallo87@gmail.com>
2016-12-08 21:39:31 -0300

    fix struct and adding people data

--- main.go
+++ main.go
@@ -7,16 +7,16 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Person struc {
-    ID string `json:"id,omitempty"`
-    FirstName string `json:"firstname,omitempty"`
-    LastName string `json:"lastname,omitempty"`
-    Address *Address `json:"address,omitempty"`
+type Person struct {
+	ID        string   `json:"id,omitempty"`
+	FirstName string   `json:"firstname,omitempty"`
+	LastName  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
 }
 
-type Address struc {
-    City string `json:"city,omitempty"`
-    State string `json:"state,omitempty"`
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
 }
 
 var people []Person
@@ -39,6 +39,7 @@ func DeletePersonEndpoint(w http.ResponseWritter, req *http.Request) {
 
 func main() {
 	router := mux.NewRouter()
+	people = append(people, Person{ID: "1", FirstName: "Carlos", Lastname: "Carv", Address: &Address{City: "Asuncion", State: "Central"}})
 	router.HandleFunc("/people", GetPeopleEndpoint).Method("GET")
 	router.HandleFunc("/people/{id}", GetPersonEndpoint).Method("GET")
 	router.HandleFunc("/people/{id}", CreatePersonEndpoint).Method("POST")
