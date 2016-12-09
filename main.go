95fdbee9dc5faeb5fb28f9233cf8bba9338a8aa1
carloscarvallo <carloscarvallo87@gmail.com>
2016-12-08 21:35:35 -0300

    handle routes and structs for data

--- main.go
+++ main.go
@@ -7,6 +7,20 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Person struc {
+    ID string `json:"id,omitempty"`
+    FirstName string `json:"firstname,omitempty"`
+    LastName string `json:"lastname,omitempty"`
+    Address *Address `json:"address,omitempty"`
+}
+
+type Address struc {
+    City string `json:"city,omitempty"`
+    State string `json:"state,omitempty"`
+}
+
+var people []Person
+
 func GetPersonEndpoint(w http.ResponseWritter, req *http.Request) {
 
 }
@@ -25,5 +39,9 @@ func DeletePersonEndpoint(w http.ResponseWritter, req *http.Request) {
 
 func main() {
 	router := mux.NewRouter()
+	router.HandleFunc("/people", GetPeopleEndpoint).Method("GET")
+	router.HandleFunc("/people/{id}", GetPersonEndpoint).Method("GET")
+	router.HandleFunc("/people/{id}", CreatePersonEndpoint).Method("POST")
+	router.HandleFunc("/people/{id}", DeletePersonEndpoint).Method("DELETE")
 	log.Fatal(http.ListenAndServe(":12345", router))
 }
