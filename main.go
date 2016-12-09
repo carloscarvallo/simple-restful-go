5a535b752f7423661043af55da9711d9b3b442f5
carloscarvallo <carloscarvallo87@gmail.com>
2016-12-08 21:47:18 -0300

    fix :D

--- main.go
+++ main.go
@@ -23,30 +23,30 @@ type Address struct {
 
 var people []Person
 
-func GetPersonEndpoint(w http.ResponseWritter, req *http.Request) {
+func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
 
 }
 
-func GetPeopleEndpoint(w http.ResponseWritter, req *http.Request) {
+func GetPeopleEndpoint(w http.ResponseWriter, req *http.Request) {
 	// taking the slices of strucs of people and encoding it to JSON
 	json.NewEncoder(w).Encode(people)
 }
 
-func CreatePersonEndpoint(w http.ResponseWritter, req *http.Request) {
+func CreatePersonEndpoint(w http.ResponseWriter, req *http.Request) {
 
 }
 
-func DeletePersonEndpoint(w http.ResponseWritter, req *http.Request) {
+func DeletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
 
 }
 
 func main() {
 	router := mux.NewRouter()
-	people = append(people, Person{ID: "1", FirstName: "Carlos", Lastname: "Carv", Address: &Address{City: "Asuncion", State: "Central"}})
-	people = append(people, Person{ID: "2", FirstName: "Maria", Lastname: "Mont"})
-	router.HandleFunc("/people", GetPeopleEndpoint).Method("GET")
-	router.HandleFunc("/people/{id}", GetPersonEndpoint).Method("GET")
-	router.HandleFunc("/people/{id}", CreatePersonEndpoint).Method("POST")
-	router.HandleFunc("/people/{id}", DeletePersonEndpoint).Method("DELETE")
+	people = append(people, Person{ID: "1", FirstName: "Carlos", LastName: "Carv", Address: &Address{City: "Asuncion", State: "Central"}})
+	people = append(people, Person{ID: "2", FirstName: "Maria", LastName: "Mont"})
+	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
+	router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
+	router.HandleFunc("/people/{id}", CreatePersonEndpoint).Methods("POST")
+	router.HandleFunc("/people/{id}", DeletePersonEndpoint).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":12345", router))
 }
